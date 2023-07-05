6a779647bcb2ec829cd4a0d411db7e54efb989da
root <1874315714@qq.com>
2023-07-05 10:33:31 +0800

    hub提交

--- src/main/java/cn/goldencis/auxiliary/domain/deploy/Hub.java
+++ src/main/java/cn/goldencis/auxiliary/domain/deploy/Hub.java
@@ -4,10 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "hub")
+@Entity
 public class Hub {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private Boolean state;
     private String path;
--- src/main/java/cn/goldencis/auxiliary/domain/deploy/repository/HubRepository.java
+++ src/main/java/cn/goldencis/auxiliary/domain/deploy/repository/HubRepository.java
@@ -1,7 +1,10 @@
 package cn.goldencis.auxiliary.domain.deploy.repository;
 
+import cn.goldencis.auxiliary.domain.deploy.Hub;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HubRepository {
+public interface HubRepository extends JpaRepository<Hub, Long>, QuerydslPredicateExecutor<Hub> {
 }
